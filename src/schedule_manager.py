71c6cdfd0ca5989571544fa3d74d0183f50ec840
Lasse Poulsen <work.lasse.face@gmail.com>
2022-02-17 22:02:48 +0100

    Finish getIcs method to get json from cache

--- src/schedule_manager.py
+++ src/schedule_manager.py
@@ -1,6 +1,42 @@
-class ScheduleManager:
-    def __init__(self) -> None:
-        pass
+import json
+from typing import Dict, List
+import ics_service
+import re
 
-    def getIcs() -> None:
-        pass
+PATTERN1 = re.compile("\s+")  # noqa W605
+PATTERN2 = re.compile(",+")
+PATTERN3 = re.compile(r"(?:(?<=\s)|^)(?:[a-z]|\d+)", re.I)
+
+
+class ScheduleManager:
+    __events: List = []
+    __json: Dict = {}
+
+    def __init__(self, id: str) -> None:
+        self.json = self.getIcs(id)
+
+    def getIcs(self, id: str) -> str:
+        try:
+            cachePath = "cache/" + id + ".json"
+            with open(cachePath) as icsFile:
+                jsonObj = json.load(icsFile)
+        except FileNotFoundError:
+            ics_service.cacheIcs(id)
+
+        return jsonObj
+
+    @property
+    def events(self) -> List:
+        return self.__events
+
+    @events.setter
+    def events(self, events: List):
+        self.__events = events
+
+    @property
+    def json(self) -> Dict:
+        return self.__json
+
+    @json.setter
+    def json(self, data: Dict):
+        self.__json = data
