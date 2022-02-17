3c63674cddac33232ed09c836d31dd5042fe9fb9
Lasse Poulsen <work.lasse.face@gmail.com>
2022-02-18 00:07:16 +0100

    Finish event parser and JSON formatter

--- src/ics_service.py
+++ src/ics_service.py
@@ -1,9 +1,13 @@
+from enum import Enum
 import json
-from typing import List
+from typing import Dict, List, Tuple
 import urllib3
 import urllib3.exceptions as url_except
 import datetime as dt  # noqa # pylint: disable=unused-import
 from icalendar import Calendar
+import re
+import dateutil.parser as dateParse
+import os
 
 
 def main():
@@ -16,20 +20,23 @@ def main():
 
 
 def cacheIcs(id):
+    if not os.path.exists("cache"):
+        os.makedirs("cache")
+
     icsString: str = __fetchIcsFile(id)
     icsList: List[str] = __parseIcs(icsString)
-    icsJson: str = __listToJson(id, icsList)
-    __saveToCache(icsJson)
+    icsJson: str = __listToJson(icsList)
+    print(icsJson)
+    __saveToCache(id, icsJson)
 
 
 def __fetchIcsFile(id) -> str:
-    kronoxURL = "https://kronox.hkr.se/setup/jsp/Schema.jsp?startDatum=idag&intervallTyp=m&intervallAntal=6&sprak=EN&sokMedAND=true&forklaringar=true&resurser="  # noqa: E501
-    schemaURL = "https://schema.hkr.se/setup/jsp/Schema.jsp?startDatum=idag&intervallTyp=m&intervallAntal=6&sprak=EN&sokMedAND=true&forklaringar=true&resurser="  # noqa: E501
+    kronoxURL = "https://kronox.hkr.se/setup/jsp/SchemaICAL.ics?startDatum=idag&intervallTyp=m&intervallAntal=6&sprak=SV&sokMedAND=true&forklaringar=true&resurser="  # noqa: E501
+    schemaURL = "https://schema.hkr.se/setup/jsp/SchemaICAL.ics?startDatum=idag&intervallTyp=m&intervallAntal=6&sprak=SV&sokMedAND=true&forklaringar=true&resurser="  # noqa: E501
     http = urllib3.PoolManager()
 
     try:
         res = http.request("GET", kronoxURL + id)
-        print(res.data)
     except url_except.TimeoutError or url_except.ConnectionError:
         try:
             res = http.request("GET", schemaURL + id)
@@ -44,40 +51,83 @@ def __parseIcs(ics: str) -> List[str]:
     ical = Calendar.from_ical(ics)
     for i, component in enumerate(ical.walk()):
         if component.name == "VEVENT":
-
             event = {}
-            event["start"] = {
-                "dateTime": component.get("dtstart").dt.isoformat(),
-                "timeZone": str(component.get("dtstart").dt.tzinfo),
-            }
-            event["end"] = {
-                "dateTime": component.get("dtend").dt.isoformat(),
-                "timeZone": str(component.get("dtend").dt.tzinfo),
-            }
-            event["title"] = component.get("summary")
-            event["location"] = component.get("location")
-            event["sequence"] = component.get("sequence")
-            try:
-                desc = component.get("description").replace("\xa0", " ")
-                if "description" in event:
-                    event["description"] = desc + "\r\n" + event["description"]
-                else:
-                    event["description"] = desc
+            title, course, lecturer = __titleSplitter(component.get("summary"))
 
-                event["description"] = component.get("description")
-            except AttributeError:
-                pass
+            event["start"] = component.get("dtstart").dt.isoformat()
+            event["end"] = component.get("dtend").dt.isoformat()
+            event["course"] = course
+            event["lecturer"] = lecturer
+            event["location"] = str(component.get("location"))
+            event["title"] = title
 
             events.append(event)
 
     return events
 
 
+def __titleSplitter(title: str) -> Tuple[str, str, str]:
+    PATTERN1 = re.compile("\s+")  # noqa W605
+    PATTERN2 = re.compile(",+")
+
+    split_name = re.split("Kurs.grp: | Sign: | Moment: | Program: ", title)
+    edit_name = split_name[3]
+
+    edit_name = edit_name.replace(":  :", ":")
+    edit_name = edit_name.rstrip(" : ")
+    for j in PATTERN1.findall(split_name[3]):
+        edit_name = edit_name.replace(j, " ")
+    for j in PATTERN2.findall(split_name[3]):
+        edit_name = edit_name.replace(j, ",")
+    edit_name = edit_name.replace(";", "")
+
+    return (edit_name, split_name[1], split_name[2])
+
+
 def __listToJson(events: List) -> str:
-    pass
+    eventsDict = {}
+
+    for event in events:
+        eventDate = dateParse.isoparse(event["start"])
+
+        if str(eventDate.year) not in eventsDict.keys():
+            eventsDict[str(eventDate.year)] = {}
+
+        yearDict: Dict = eventsDict[str(eventDate.year)]
+
+        if months(eventDate.month).name not in yearDict.keys():
+            yearDict[months(eventDate.month).name] = {}
+
+        monthDict: Dict = yearDict[months(eventDate.month).name]
+
+        if str(eventDate.day) not in monthDict.keys():
+            monthDict[str(eventDate.day)] = []
+
+        monthDict[str(eventDate.day)].append(event)
+
+    return json.dumps(eventsDict)
 
 
 def __saveToCache(id: str, data: str) -> None:
     filePath = "cache/" + id + ".json"
     with open(filePath, "w") as f:
         f.write(data)
+
+
+class months(Enum):
+    January = 1
+    February = 2
+    March = 3
+    April = 4
+    May = 5
+    June = 6
+    July = 7
+    August = 8
+    September = 9
+    October = 10
+    November = 11
+    December = 12
+
+
+if __name__ == "__main__":
+    main()
