fba0d319c4920afd9ff816d2d988617ade9ba2e4
Lasse Poulsen <work.lasse.face@gmail.com>
2022-02-17 22:01:51 +0100

    Add basic schedule API endpoint
    Basic endpoint for getting a schedule in cache by id

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from schedule_manager import ScheduleManager
 
 app = FastAPI()
 
@@ -10,4 +11,5 @@ async def root1():
 
 @app.get("/schedule/{id}")
 async def scheduleGetter(id: str):
-    return {"message": "Hello World"}
+    schedule = ScheduleManager(id)
+    return schedule.json
