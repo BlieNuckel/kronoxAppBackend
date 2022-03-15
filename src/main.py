155ff5a9d1d6c0d475524e314d02b4a7cda72e2f
Lasse Poulsen <work.lasse.face@gmail.com>
2022-03-15 02:01:08 +0100

    Add more schools

--- src/main.py
+++ src/main.py
@@ -7,12 +7,28 @@ from src.kronox_scraper.kronox_scrape import runKronoxSearch
 
 app = FastAPI()
 
-VALID_SCHOOLS = ["hkr", "mau", "oru", "ltu"]
+VALID_SCHOOLS = [
+    "hkr",
+    "mau",
+    "oru",
+    # "ltu",
+    "hig",
+    # "sh",
+    "hv",
+    "hb",
+    "mdh",
+]
 SCHOOL_BASE_URLS = {
     "hkr": "schema.hkr.se",
     "mau": "schema.mau.se",
     "oru": "schema.oru.se",
-    "ltu": "tenta.ltu.se",
+    # ! "ltu": "schema.ltu.se", COMPLETELY BLOCKED OFF BECAUSE LOGIN REQUIRED
+    "hig": "schema.hig.se",
+    # ! "sh": "kronox.sh.se", COMPLETELY BLOCKED OFF BECAUSE LOGIN REQUIRED
+    "hv": "schema.hv.se",
+    "hb": "schema.hb.se",
+    "mdh": "webbschema.mdh.se",  # ! LOGIN REQUIRED FOR PROGRAMMES
+    # Konstfack school on kronox' website, but can't find schedule page
 }
 
 
