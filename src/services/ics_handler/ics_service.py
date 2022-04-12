28ee76049c8606ed70866421ace96133c1d375d6
adisve <adis.veletanlic@gmail.com>
2022-04-12 20:07:28 +0200

    Add comparison checking and appropriate uuid updates

--- src/services/ics_handler/ics_service.py
+++ src/services/ics_handler/ics_service.py
@@ -10,7 +10,8 @@ def getIcs(id: str, baseUrl: str, startDate: str = None) -> Dict:
     if not isinstance(icsFile, bytes):
         print("ICS FILE NOT BYTES - ics_service.py:11")
         raise TypeError
-    icsList: List[Dict] = ics_utils._parseIcs(icsFile)
+    
+    icsList, uuid_list = ics_utils._parseIcs(icsFile)
     icsDict: Dict = ics_utils._listToJson(icsList)
 
     scheduleObject = {}
@@ -19,17 +20,19 @@ def getIcs(id: str, baseUrl: str, startDate: str = None) -> Dict:
     scheduleObject["baseUrl"] = baseUrl
     scheduleObject["schedule"] = icsDict
     scheduleObject["startsAt"] = startDate
+    scheduleObject["generated_uuids"] = uuidList
 
-    return scheduleObject
+    return scheduleObjectz
 
 
 def getAndCacheIcs(id: str, baseUrl: str, startDateTag: StartDateEnum, returnDict: bool = True) -> Dict | None:
     icsFile: bytes = ics_utils._fetchIcsFile(id, baseUrl, startDateTag=startDateTag)
     if not isinstance(icsFile, bytes):
-        print("ICS FILE NOT BYTES - ics_service.py:28")
+        print("ICS FILE NOT BYTES - ics_service.py:32")
         raise TypeError
-    icsList: List[Dict] = ics_utils._parseIcs(icsFile)
+    icsList, uuid_list = ics_utils._parseIcs(icsFile)
     icsDict: Dict = ics_utils._listToJson(icsList)
-    scheduleObject: Dict = ics_utils._saveToCache(id, icsDict, baseUrl, startDateTag)
+    scheduleObject: Dict = ics_utils._saveToCache(id, icsDict, baseUrl, startDateTag, uuid_list)
+
     if returnDict:
         return scheduleObject
--- src/services/ics_handler/ics_utils.py
+++ src/services/ics_handler/ics_utils.py
@@ -1,4 +1,5 @@
 import time
+import uuid
 from typing import Dict, List, Tuple
 import urllib3
 import urllib3.exceptions as url_except
@@ -11,7 +12,7 @@ from src.services.mongo_connector import MongoConnector
 import src.util.course_color as color
 from src.util.enums import StartDateEnum, days, months
 
-
+"""Run GET request on specific schedule id and return result string"""
 def _fetchIcsFile(id: str, baseUrl: str, startDateTag: StartDateEnum = None, startDate: str = None) -> str | None:
     urlStartDate = ""
 
@@ -27,7 +28,6 @@ def _fetchIcsFile(id: str, baseUrl: str, startDateTag: StartDateEnum = None, sta
         res = http.request("GET", schemaURL + id)
 
         if "VEVENT" not in str(res.data):
-            print("DATA FETCHED HAS NO 'VEVENT' TAG - ics_utils.py:30")
             raise TypeError
     except url_except.TimeoutError or url_except.ConnectionError or TypeError:
         pass
@@ -35,12 +35,14 @@ def _fetchIcsFile(id: str, baseUrl: str, startDateTag: StartDateEnum = None, sta
     return res.data
 
 
-def _parseIcs(ics: bytes) -> List[Dict]:
+def _parseIcs(ics: bytes) -> List[Dict] and List[int]:
     events = []
     ical = Calendar.from_ical(ics)
+    uuid_list = []
     for i, component in enumerate(ical.walk()):
         if component.name == "VEVENT":
             event = {}
+            temp_uuid = getUniqueScheduleChannelId()
             title, course, lecturer = _titleSplitter(component.get("summary"))
 
             event["start"] = component.get("dtstart").dt.isoformat()
@@ -50,11 +52,52 @@ def _parseIcs(ics: bytes) -> List[Dict]:
             event["location"] = str(component.get("location"))
             event["title"] = title
             event["color"] = color.getColor(course)
-
+            event["channel_id"] = temp_uuid
+            
+            uuid_list.append(temp_uuid)
             events.append(event)
+    ## List of dictionaries
+    return events, uuid_list
 
-    return events
 
+def _parseCompareIcs(ics: bytes, generated_uuids, currentEventsList) -> List[Dict] and List[int]:
+    ## Parse list of previous uuids and assign them incrementally
+    ## to the events in the new updated schedule, up until they
+    ## run out. Then we append new uuids.
+    events = []
+    ical = Calendar.from_ical(ics)
+    flag = False
+
+    ## List of remaining events
+    for i, component in enumerate(ical.walk()):
+        ## Check if length of new ical does not overflow
+        ## current collection of uuids
+        if component.name == "VEVENT":
+            if i > len(generated_uuids):
+                flag = True
+                events.append(_createEvent(component, flag, generated_uuids, i))
+                continue
+            events.append(_createEvent(component, flag, generated_uuids, i))
+            if(events[i]['start'] != currentEventsList[i]['start'] | events[i]['end'] != currentEventsList[i]['end']):
+                events[i]['start'] = int(str(events[i]['start'])[:-4]+'9999')
+                generated_uuids[i] = events[i]['start']
+            
+    return events, scheduleJson["generated_uuids"]
+
+def _createEvent(component, flag, uuids, i):
+    
+    ## Utility function for creating an event object
+    event = {}
+    title, course, lecturer = _titleSplitter(component.get("summary"))
+    event["start"] = component.get("dtstart").dt.isoformat()
+    event["end"] = component.get("dtend").dt.isoformat()
+    event["course"] = course
+    event["lecturer"] = lecturer
+    event["location"] = str(component.get("location"))
+    event["title"] = title
+    event["color"] = color.getColor(course)
+    event["channel_id"] = getUniqueScheduleChannelId() if flag else uuids[i]
+    return event
 
 def _titleSplitter(title: str) -> Tuple[str, str, str]:
 
@@ -99,7 +142,7 @@ def _titleSplitter(title: str) -> Tuple[str, str, str]:
 def _listToJson(events: List[Dict]) -> Dict:
     eventsDict = {}
 
-    for event in events:
+    for i, event in enumerate(events):
         eventDate = dateParser.isoparse(event["start"])
 
         if str(eventDate.year) not in eventsDict.keys():
@@ -125,17 +168,21 @@ def _listToJson(events: List[Dict]) -> Dict:
     return eventsDict
 
 
-def _saveToCache(id: str, data: Dict, baseUrl: str, startDateTag: StartDateEnum) -> Dict:
-
+def _saveToCache(id: str, data: Dict, baseUrl: str, startDateTag: StartDateEnum, uuid_list: List[int]) -> Dict:
     scheduleJson = {}
     scheduleJson["_id"] = {"scheduleId": id, "startsAt": startDateTag.value}
     scheduleJson["cachedAt"] = time.ctime()
     scheduleJson["baseUrl"] = baseUrl
     scheduleJson["startsAt"] = startDateTag.value
     scheduleJson["schedule"] = data
+    scheduleJson["generated_uuids"] = uuid_list
 
     MongoConnector.updateOne(
         "schedules", {"_id": {"scheduleId": id, "startsAt": startDateTag.value}}, scheduleJson, True
     )
 
     return scheduleJson
+
+def getUniqueScheduleChannelId():
+    ## Right shift by 64 bits to avoid 8 byte int limit in MongoDb
+    return (uuid.uuid1().int>>64)/2
